83687aea7cbaa79ec660a7698e7c8625ed940569
wangqiulin <wangqiulin@netwisdom.cn>
2018-05-16 20:18:53 +0800

    分布式锁

--- wql-cloud-bizserviceapp/bizserviceapp-user/src/main/java/com/wql/cloud/wqlcloud/controller/UserController.java
+++ wql-cloud-bizserviceapp/bizserviceapp-user/src/main/java/com/wql/cloud/wqlcloud/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.wql.cloud.wqlcloud.controller;
 
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wql.cloud.wqlcloud.client.order.OrderClient;
+import com.wql.cloud.wqlcloud.redis.lock.AquiredLockWorker;
+import com.wql.cloud.wqlcloud.redis.lock.RedisLocker;
 
 /**
  *
@@ -38,13 +43,75 @@ public class UserController {
     }
 	
 	
-	
+	/**
+	 * POST方式刷新配置值: http://localhost:8881/bus/refresh
+	 */
 	@Value("${name}")
 	String configName;
 	
-	@RequestMapping(value = "/hello",method = RequestMethod.GET)
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String sayHello(){
         return configName;
     }
+	
+	
+	@Autowired
+    RedisLocker distributedLocker;
+	
+    @RequestMapping(value = "/redlock", method = RequestMethod.GET)
+    public String testRedlock() throws Exception{
+        CountDownLatch startSignal = new CountDownLatch(1);
+        CountDownLatch doneSignal = new CountDownLatch(5);
+        for (int i = 0; i < 5; ++i) { // create and start threads
+            new Thread(new Worker(startSignal, doneSignal)).start();
+        }
+        startSignal.countDown(); // let all threads proceed
+        doneSignal.await();
+        System.out.println("All processors done. Shutdown connection");
+        return "redlock";
+    }
+
+    class Worker implements Runnable {
+        private final CountDownLatch startSignal;
+        private final CountDownLatch doneSignal;
+
+        Worker(CountDownLatch startSignal, CountDownLatch doneSignal) {
+            this.startSignal = startSignal;
+            this.doneSignal = doneSignal;
+        }
+
+        public void run() {
+            try {
+                startSignal.await();
+                distributedLocker.lock("test",new AquiredLockWorker<Object>() {
+
+                    @Override
+                    public Object invokeAfterLockAquire() {
+                        doTask();
+                        return null;
+                    }
+
+                });
+            }catch (Exception e){
+
+            }
+        }
+
+        void doTask() {
+            System.out.println(Thread.currentThread().getName() + " start");
+            Random random = new Random();
+            int _int = random.nextInt(200);
+            System.out.println(Thread.currentThread().getName() + " sleep " + _int + "millis");
+            try {
+                Thread.sleep(_int);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " end");
+            doneSignal.countDown();
+        }
+    }
+	
+	
 	
 }
