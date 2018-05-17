aa73056a20053d6e56c7a74d74681552f9e2ed78
wangqiulin <wangqiulin@netwisdom.cn>
2018-05-17 09:34:07 +0800

    分布式锁

---
+++ wql-cloud-basic/basic-redis/src/main/java/com/wql/cloud/wqlcloud/redis/lock/UnableToAquireLockException.java
@@ -0,0 +1,26 @@
+package com.wql.cloud.wqlcloud.redis.lock;
+/**
+ * 异常类
+ * 
+ * @author wangqiulin
+ * @date 2018年5月16日
+ */
+public class UnableToAquireLockException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UnableToAquireLockException() {
+    }
+
+    public UnableToAquireLockException(String message) {
+        super(message);
+    }
+
+    public UnableToAquireLockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
