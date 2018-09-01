8af34891585aabb5198dc771ad10bc72e2828b31
wangqiulin <wangqiulin@netwisdom.cn>
2018-09-01 18:20:34 +0800

    save

--- wql-cloud-comapp/comapp-config/src/main/java/com/wql/cloud/server/config/ConfigServerApplication.java
+++ wql-cloud-comapp/comapp-config/src/main/java/com/wql/cloud/server/config/ConfigServerApplication.java
@@ -1,23 +1,29 @@
 package com.wql.cloud.server.config;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
- *
+ * 1，@EnableDiscoveryClient注解是基于spring-cloud-commons依赖，并且在classpath中实现； 
+   2，@EnableEurekaClient注解是基于spring-cloud-netflix依赖，只能为eureka作用；
+       如果你的classpath中添加了eureka，则它们的作用是一样的。
+ * 
  * @author wangqiulin
  * @date 2018年5月16日
  */
 @SpringBootApplication
-//@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigServerApplication {
 
 	 public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        new SpringApplicationBuilder(ConfigServerApplication.class)
+        	.web(true)
+        	.bannerMode(Banner.Mode.OFF)
+        	.run(args);
      }
 	
 }
--- wql-cloud-comapp/comapp-eureka/src/main/java/com/wql/cloud/server/eureka/EurekaServerApplication.java
+++ wql-cloud-comapp/comapp-eureka/src/main/java/com/wql/cloud/server/eureka/EurekaServerApplication.java
@@ -1,7 +1,8 @@
 package com.wql.cloud.server.eureka;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -14,7 +15,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        new SpringApplicationBuilder(EurekaServerApplication.class)
+        	.web(true)
+        	.bannerMode(Banner.Mode.OFF)
+        	.run(args);
     }
 	
 }
