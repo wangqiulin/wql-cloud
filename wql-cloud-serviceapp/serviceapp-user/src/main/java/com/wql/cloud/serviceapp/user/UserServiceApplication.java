a559e748147b0a3ad2f98e48c1e072f3d037e9e7
wangqiulin <wangqiulin@netwisdom.cn>
2018-10-18 19:19:48 +0800

    注入数据源

--- wql-cloud-serviceapp/serviceapp-user/src/main/java/com/wql/cloud/serviceapp/user/UserServiceApplication.java
+++ wql-cloud-serviceapp/serviceapp-user/src/main/java/com/wql/cloud/serviceapp/user/UserServiceApplication.java
@@ -3,9 +3,13 @@ package com.wql.cloud.serviceapp.user;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
+
+import com.wql.cloud.basic.datasource.dynamic.DynamicDataSourceRegister;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Import({DynamicDataSourceRegister.class})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
