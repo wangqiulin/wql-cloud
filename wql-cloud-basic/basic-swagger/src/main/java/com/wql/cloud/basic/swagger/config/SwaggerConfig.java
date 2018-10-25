b765e22d1454adc2ad622d74a81fdb2019bb4cd8
wangqiulin <wangqiulin@netwisdom.cn>
2018-10-25 18:47:05 +0800

    basic

---
+++ wql-cloud-basic/basic-swagger/src/main/java/com/wql/cloud/basic/swagger/config/SwaggerConfig.java
@@ -0,0 +1,64 @@
+package com.wql.cloud.basic.swagger.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author wangqiulin
+ * @date 2018年5月12日
+ */
+@Configuration
+@EnableSwagger2
+@ConditionalOnExpression("${swagger.enabled:true}")
+public class SwaggerConfig {
+
+	@Value("${swagger.controller.location:}")
+	private String controllerLocation;
+	
+	@Value("${swagger.enabled:true}") 
+	private Boolean enabled;
+	
+	@Lazy
+	@Bean
+    public Docket createRestApi() {
+		//添加head参数start
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        tokenPar.name("Authorization").description("登录令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+        //添加head参数end
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(enabled) //是否开启swagger
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(controllerLocation))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(pars);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API接口")
+                .build();
+    }
+	
+}
