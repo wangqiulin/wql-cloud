b23c93464925cdf0e04b3e227859e2ea1f7a4a35
wangqiulin <wangqiulin@netwisdom.cn>
2019-01-15 15:14:32 +0800

    common 赠送改查方法

--- wql-cloud-basic/basic-datasource/src/main/java/com/wql/cloud/basic/datasource/commonService/BaseService.java
+++ wql-cloud-basic/basic-datasource/src/main/java/com/wql/cloud/basic/datasource/commonService/BaseService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.CollectionUtils;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -23,7 +22,7 @@ public abstract class BaseService<T> {
     @Autowired
     private Mapper<T> mapper;
     
-    //=================查询===================//
+    //=================查询一条===================//
     
     public T queryById(Integer id) {
         return this.mapper.selectByPrimaryKey(id);
@@ -33,17 +32,15 @@ public abstract class BaseService<T> {
         return this.mapper.selectByPrimaryKey(id);
     }
     
-    public T queryOne(T record) {
+    public T queryOneByRecord(T record) {
         return this.mapper.selectOne(record);
     }
 
-    public T queryOne(Example example) {
-        List<T> list = this.mapper.selectByExample(example);
-        if(list != null && list.size() > 1) {
-        	throw new RuntimeException("【ERROR】find more than one record");
-        }
-        return CollectionUtils.isEmpty(list) ? null : list.get(0);
+    public T queryOneByExample(Example example) {
+        return this.mapper.selectOneByExample(example);
     }
+    
+    //=================查询多条===================//
     
     public List<T> queryList() {
         return this.mapper.selectAll();
@@ -67,12 +64,8 @@ public abstract class BaseService<T> {
      * @return
      */
     public PageInfo<T> queryPageListByRecord(Integer page, Integer rows, T record) {
-    	if(page == null || page <= 0) {
-    		page = 1;
-    	}
-    	if(rows == null) {
-    		rows = 10;
-    	}
+    	page = (page == null || page <= 0) ? 1 : page;
+    	rows = (rows == null || rows <= 0) ? 10 : rows;
     	return queryPageListByRecord(page, rows, record, false);
     }
     
@@ -85,12 +78,8 @@ public abstract class BaseService<T> {
      * @return
      */
     public PageInfo<T> queryPageListByRecord(Integer page, Integer rows, T record, boolean desc) {
-    	if(page == null || page <= 0) {
-    		page = 1;
-    	}
-    	if(rows == null) {
-    		rows = 10;
-    	}
+    	page = (page == null || page <= 0) ? 1 : page;
+    	rows = (rows == null || rows <= 0) ? 10 : rows;
     	PageHelper.startPage(page, rows);
         if(desc) {
         	PageHelper.orderBy("id desc");
@@ -106,12 +95,8 @@ public abstract class BaseService<T> {
      * @return
      */
     public PageInfo<T> queryPageListByExample(Integer page, Integer rows, Example example) {
-    	if(page == null || page <= 0) {
-    		page = 1;
-    	}
-    	if(rows == null) {
-    		rows = 10;
-    	}
+    	page = (page == null || page <= 0) ? 1 : page;
+    	rows = (rows == null || rows <= 0) ? 10 : rows;
     	return queryPageListByExample(page, rows, example, false);
     }
     
@@ -124,12 +109,8 @@ public abstract class BaseService<T> {
      * @return
      */
     public PageInfo<T> queryPageListByExample(Integer page, Integer rows, Example example, boolean desc) {
-    	if(page == null || page <= 0) {
-    		page = 1;
-    	}
-    	if(rows == null) {
-    		rows = 10;
-    	}
+    	page = (page == null || page <= 0) ? 1 : page;
+    	rows = (rows == null || rows <= 0) ? 10 : rows;
         PageHelper.startPage(page, rows);
         if(desc) {
         	PageHelper.orderBy("id desc");
@@ -145,6 +126,16 @@ public abstract class BaseService<T> {
 
     public int queryCountByExample(Example example) {
         return this.mapper.selectCountByExample(example);
+    }
+    
+    //=================根据主键，查询记录是否存在===================//	
+    
+    public boolean queryExistsById(Integer id){
+    	return this.mapper.existsWithPrimaryKey(id);
+    }
+    
+    public boolean queryExistsById(Long id){
+    	return this.mapper.existsWithPrimaryKey(id);
     }
     
     //=================新增===================//
@@ -186,23 +177,17 @@ public abstract class BaseService<T> {
         return this.mapper.deleteByPrimaryKey(id);
     }
 
-    public int deleteByIds(List<Integer> ids, Class<T> clazz) {
-    	Example example = new Example(clazz);
-    	example.createCriteria().andIn("id", ids);
-        return this.mapper.deleteByExample(example);
-    }
-    
-    public int deleteByIds2(List<Long> ids, Class<T> clazz) {
-    	Example example = new Example(clazz);
-    	example.createCriteria().andIn("id", ids);
-        return this.mapper.deleteByExample(example);
-    }
-    
     public int deleteByRecord(T record) {
         return this.mapper.delete(record);
     }
     
     public int deleteByExample(Example example) {
+        return this.mapper.deleteByExample(example);
+    }
+    
+    public int deleteByIds(List<?> ids, Class<T> clazz) {
+    	Example example = new Example(clazz);
+    	example.createCriteria().andIn("id", ids);
         return this.mapper.deleteByExample(example);
     }
     
