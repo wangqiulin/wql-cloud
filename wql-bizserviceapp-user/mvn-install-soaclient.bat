8d98f911d466b1154fbdcaba95553b9c49218980
wangqiulin <wangqiulin@netwisdom.cn>
2019-03-26 17:25:37 +0800

    update user结构

---
+++ wql-bizserviceapp-user/mvn-install-soaclient.bat
@@ -0,0 +1,8 @@
+@echo off
+echo [wql cloud] install client jar
+cd /D %~dp0
+set MAVEN_OPTS= -Xms256M -Xmx1024M -XX:PermSize=64M -XX:MaxPermSize=128M -Dfile.encoding=UTF-8
+call mvn clean assembly:assembly
+cd target
+call mvn install:install-file -Dfile=bizserviceapp-user-0.0.1-SNAPSHOT.jar -DgroupId=com.wql.cloud -DartifactId=wql-soaclient-user -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
+pause
