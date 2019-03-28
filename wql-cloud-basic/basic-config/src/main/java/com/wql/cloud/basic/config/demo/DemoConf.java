5c0bed6757bf070ff671add72a2f076854d6c363
wangqiulin <wangqiulin@netwisdom.cn>
2019-03-28 18:34:53 +0800

    新增示例

---
+++ wql-cloud-basic/basic-config/src/main/java/com/wql/cloud/basic/config/demo/DemoConf.java
@@ -0,0 +1,16 @@
+package com.wql.cloud.basic.config.demo;
+
+import org.springframework.stereotype.Component;
+
+import com.xxl.conf.core.annotation.XxlConf;
+
+/**
+ *  测试示例
+ */
+@Component
+public class DemoConf {
+
+	@XxlConf("default.key02")
+	public String paramByAnno;
+	
+}
