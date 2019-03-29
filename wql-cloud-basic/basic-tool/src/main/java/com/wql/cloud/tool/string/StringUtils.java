db564dc30cd68ab2db891351a135f7b589b6cacf
wangqiulin <wangqiulin@netwisdom.cn>
2019-03-29 10:13:36 +0800

    修改

--- wql-cloud-basic/basic-tool/src/main/java/com/wql/cloud/tool/string/StringUtils.java
+++ wql-cloud-basic/basic-tool/src/main/java/com/wql/cloud/tool/string/StringUtils.java
@@ -1,52 +1,8 @@
 package com.wql.cloud.tool.string;
 
 import java.io.UnsupportedEncodingException;
-import java.text.MessageFormat;
 
 public class StringUtils extends org.apache.commons.lang3.StringUtils {
-	
-    /**
-     * 格式化字符串
-     *
-     * @param format      格式
-     * @param argNullable 参数是否可以为空
-     * @param args        参数列表
-     * @return 格式化后的字符串
-     */
-    private static String format(String format, boolean argNullable, Object... args) {
-        if (args == null || args.length == 0) {
-            return format;
-        }
-        String[] strArgs = new String[args.length];
-        for (int i = 0; i < args.length; i++) {
-            String argStr = "";
-            if (args[i] != null) {
-                argStr = String.valueOf(args[i]);
-            } else {
-                if (argNullable == false) {
-                    throw new IllegalArgumentException(MessageFormat.format(
-                            "StringUtils.format arg cannot be null。 format:{0}, args:{1}",
-                            format, JsonUtils.toJsonString(args)));
-                }
-            }
-            strArgs[i] = argStr;
-        }
-        return MessageFormat.format(format, strArgs);
-    }
-
-    /**
-     * 格式化字符串
-     * <p>
-     * 注意: 直接用MessageFormat.format数字会带逗号, 用此方法不会
-     *
-     * @param format 格式, 例如book-{0}-{1}, {0}和{1}分别是第一个和第二个参数
-     * @param args   参数列表，必须和format里面的参数标识符个数一致， 参数值不能为null, 否则会抛出异常
-     * @return 格式化后的字符串
-     * @throws IllegalArgumentException 参数为空时抛出
-     */
-    public static String format(String format, Object... args) {
-        return format(format, false, args);
-    }
 
     /**
      * 手机号去敏(隐藏中间四位,若传入参数格式有误原样返回)
