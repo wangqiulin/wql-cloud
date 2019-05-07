9988d669d42638b724db52f06985571279d10c4d
wangqiulin <wangqiulin@netwisdom.cn>
2019-05-07 20:05:01 +0800

    seata分布式事务

--- wql-bizserviceapp-user/bizserviceapp-user/src/main/java/com/wql/cloud/userservice/service/impl/UserServiceImpl.java
+++ wql-bizserviceapp-user/bizserviceapp-user/src/main/java/com/wql/cloud/userservice/service/impl/UserServiceImpl.java
@@ -2,14 +2,19 @@ package com.wql.cloud.userservice.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 import com.github.pagehelper.PageInfo;
 import com.wql.cloud.basic.datasource.baseservice.BaseService;
 import com.wql.cloud.basic.datasource.dynamic.TargetDataSource;
+import com.wql.cloud.payservice.client.PayClient;
+import com.wql.cloud.payservice.pojo.domain.Order;
 import com.wql.cloud.userservice.pojo.domain.User;
 import com.wql.cloud.userservice.service.UserService;
+
+import io.seata.spring.annotation.GlobalTransactional;
 
 /**
  * 分布式锁： @DistributedLock(lockName="lock:test", tryLock=true)
@@ -20,8 +25,17 @@ import com.wql.cloud.userservice.service.UserService;
 @Service
 public class UserServiceImpl extends BaseService<User> implements UserService {
 
+	@Autowired
+	private PayClient payClient;
+	
 	@Override
+	@GlobalTransactional
 	public Integer save(User req) {
+		Order order = new Order();
+		order.setOrderNo("111000");
+		order.setGoodsName("测试分布式事务");
+		payClient.save(order);
+		int i = 1/0;
 		return this.saveSelective(req);
 	}
 
---
+++ wql-cloud-basic/basic-seata/src/main/java/com/wql/cloud/basic/seata/config/SeataConfig.java
@@ -0,0 +1,27 @@
+package com.wql.cloud.basic.seata.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.seata.spring.annotation.GlobalTransactionScanner;
+
+@Configuration
+public class SeataConfig {
+
+	@Value("${spring.application.name}")
+	private String applicationId;
+
+	/**
+	 * 注册一个StatViewServlet
+	 *
+	 * @return global transaction scanner
+	 */
+	@Bean
+	public GlobalTransactionScanner globalTransactionScanner() {
+        GlobalTransactionScanner globalTransactionScanner = new GlobalTransactionScanner(
+        		applicationId, "tx_group");
+		return globalTransactionScanner;
+	}
+	
+}
