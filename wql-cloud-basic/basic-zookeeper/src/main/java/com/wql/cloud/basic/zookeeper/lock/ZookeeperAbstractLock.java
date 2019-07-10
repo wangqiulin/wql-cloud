2d8aedefeb63a7e0fef1b11604b5b07fd6d884b7
wangqiulin <wangqiulin@netwisdom.cn>
2019-07-10 19:23:51 +0800

    修改

--- wql-cloud-basic/basic-zookeeper/src/main/java/com/wql/cloud/basic/zookeeper/lock/ZookeeperAbstractLock.java
+++ wql-cloud-basic/basic-zookeeper/src/main/java/com/wql/cloud/basic/zookeeper/lock/ZookeeperAbstractLock.java
@@ -1,9 +1,13 @@
 package com.wql.cloud.basic.zookeeper.lock;
 
 import org.I0Itec.zkclient.ZkClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class ZookeeperAbstractLock implements Lock {
 
+	public final Logger logger = LoggerFactory.getLogger(this.getClass()); 
+	
 	protected static final String CONNECT_ADDRES = "192.168.1.90:2181,192.168.1.91:2181,192.168.1.92:2181";
 	
 	protected static final int SESSIONTIME = 2000;
@@ -15,7 +19,9 @@ public abstract class ZookeeperAbstractLock implements Lock {
 	public void getLock() {
 		if (tryLock()) {
 			//  获取
-			System.out.println(Thread.currentThread().getName() + "-- get lock");
+			if(logger.isDebugEnabled()) {
+				logger.debug(Thread.currentThread().getName() + "-- get lock");
+			}
 		} else {
 			waitLock();
 			getLock();
@@ -30,6 +36,9 @@ public abstract class ZookeeperAbstractLock implements Lock {
 		try {
 			if (zkClient != null) {
 				zkClient.close();
+			}
+			if(logger.isDebugEnabled()) {
+				logger.debug(Thread.currentThread().getName() + "-- unLock");
 			}
 		} catch (Exception e) {
 
