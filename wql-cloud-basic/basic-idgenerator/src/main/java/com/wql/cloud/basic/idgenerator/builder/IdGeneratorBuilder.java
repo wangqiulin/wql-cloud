7329b126974c10e4667d44de80b98696f1ccb2e5
wangqiulin <wangqiulin@netwisdom.cn>
2019-07-25 16:09:13 +0800

    删除idgen

--- wql-cloud-basic/basic-idgenerator/src/main/java/com/wql/cloud/basic/idgenerator/builder/IdGeneratorBuilder.java
+++
@@ -1,7 +0,0 @@
-package com.wql.cloud.basic.idgenerator.builder;
-
-import com.wql.cloud.basic.idgenerator.generator.IdGenerator;
-
-public interface IdGeneratorBuilder {
-	public IdGenerator build();
-}
--- wql-cloud-basic/basic-idgenerator/src/main/java/com/wql/cloud/basic/idgenerator/generator/IdGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.wql.cloud.basic.idgenerator.generator;
-
-public interface IdGenerator {
-	
-	long gen();
-	
-	long[] gen(int count);
-	
-}
