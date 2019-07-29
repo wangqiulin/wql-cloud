cff407cf6e57205df5e76be18cd104a6dcf813c7
wangqiulin <wangqiulin@netwisdom.cn>
2019-07-29 16:20:07 +0800

    nothing

--- wql-bizserviceapp-user/bizserviceapp-user/src/main/java/com/wql/cloud/userservice/mapper/UserMapper.java
+++ wql-bizserviceapp-user/bizserviceapp-user/src/main/java/com/wql/cloud/userservice/mapper/UserMapper.java
@@ -1,5 +1,10 @@
 package com.wql.cloud.userservice.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 import com.wql.cloud.basic.datasource.tk.MyMapper;
 import com.wql.cloud.userservice.pojo.domain.User;
 
@@ -9,5 +14,17 @@ import com.wql.cloud.userservice.pojo.domain.User;
  *
  */
 public interface UserMapper extends MyMapper<User>{
+	
+	/**
+	 * id 从0开始，limit表示一次多少条
+	 * 
+	 * @param id
+	 * @param limit
+	 * @return
+	 */
+	@Select({
+		"select id from t_user where id > #{id} limit #{limit}"
+	})
+	List<User> pageById(@Param("id")Long id, @Param("limit")Integer limit);
 
 }
