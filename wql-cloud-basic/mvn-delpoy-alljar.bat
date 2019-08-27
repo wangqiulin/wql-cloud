cdcdb7bc6c39f372d4e70b42128143cdba012826
wangqiulin <wangqiulin@netwisdom.cn>
2019-08-27 15:07:00 +0800

    delete

--- wql-cloud-basic/mvn-delpoy-alljar.bat
+++ wql-cloud-basic/mvn-delpoy-alljar.bat
@@ -1,8 +1,8 @@
 @echo off
-echo [intpay cloud] svn update
+echo [wisdom cloud] svn update
 cd /D %~dp0
 TortoiseProc.exe /command:update /path:"./" /closeonend:2
-echo [intpay cloud] deploy jar
+echo [wisdom cloud] deploy all jar
 set MAVEN_OPTS= -Xms256M -Xmx1024M -XX:PermSize=64M -XX:MaxPermSize=128M -Dfile.encoding=UTF-8
-call mvn clean deploy -B -e -U -Dmaven.repo.local=本地仓库路径
+call mvn clean deploy -U
 pause
