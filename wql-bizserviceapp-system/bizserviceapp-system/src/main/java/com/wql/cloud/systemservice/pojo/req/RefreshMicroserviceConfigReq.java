3fd0636745976762a0a4d55bce678c097d56070c
wangqiulin <wangqiulin@netwisdom.cn>
2019-08-30 17:34:07 +0800

    修改

--- wql-bizserviceapp-system/bizserviceapp-system/src/main/java/com/wql/cloud/systemservice/pojo/req/RefreshMicroserviceConfigReq.java
+++ wql-bizserviceapp-system/bizserviceapp-system/src/main/java/com/wql/cloud/systemservice/pojo/req/RefreshMicroserviceConfigReq.java
@@ -2,14 +2,14 @@ package com.wql.cloud.systemservice.pojo.req;
 
 public class RefreshMicroserviceConfigReq {
 
-	private String serviceName;
+	private String serviceUrl;
 
-	public String getServiceName() {
-		return serviceName;
+	public String getServiceUrl() {
+		return serviceUrl;
 	}
 
-	public void setServiceName(String serviceName) {
-		this.serviceName = serviceName;
+	public void setServiceUrl(String serviceUrl) {
+		this.serviceUrl = serviceUrl;
 	}
-	
+
 }
--- wql-bizserviceapp-system/bizserviceapp-system/src/main/java/com/wql/cloud/systemservice/service/microserviceconfig/impl/MicroserviceConfigServiceImpl.java
+++ wql-bizserviceapp-system/bizserviceapp-system/src/main/java/com/wql/cloud/systemservice/service/microserviceconfig/impl/MicroserviceConfigServiceImpl.java
@@ -1,7 +1,5 @@
 package com.wql.cloud.systemservice.service.microserviceconfig.impl;
 
-import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
-import com.wql.cloud.systemservice.mapper.microserviceconfig.MicroserviceConfigMapper;
-import com.wql.cloud.systemservice.pojo.domain.microserviceconfig.MicroserviceConfig;
 import com.wql.cloud.systemservice.pojo.req.RefreshMicroserviceConfigReq;
 import com.wql.cloud.systemservice.service.microserviceconfig.MicroserviceConfigService;
 import com.wql.cloud.tool.httpclient.HttpUtil;
@@ -22,20 +18,15 @@ public class MicroserviceConfigServiceImpl implements MicroserviceConfigService 
 	
 	@Autowired
 	private HttpUtil httpUtil;
-	@Autowired
-	private MicroserviceConfigMapper microserviceConfigMapper;
 	
 	@Override
 	public String refresh(RefreshMicroserviceConfigReq req) {
-		Assert.isTrue(StringUtils.isNotBlank(req.getServiceName()), "服务名不能为空");
 		String refreshContent = null;
-		//查询数据
-		MicroserviceConfig record = new MicroserviceConfig();
-		record.setServiceName(req.getServiceName());
-		List<MicroserviceConfig> list = microserviceConfigMapper.select(record);
-		for (MicroserviceConfig microserviceConfig : list) {
-			//刷新
-			refreshContent = httpUtil.jsonPost(microserviceConfig.getServiceUrl());
+		String serviceUrl = req.getServiceUrl();
+		Assert.isTrue(StringUtils.isNotBlank(serviceUrl), "url不能为空");
+		for (String url : StringUtils.split(serviceUrl, ";")) {
+			//post请求
+			refreshContent = httpUtil.jsonPost(url);
 		}
 		return refreshContent;
 	}
