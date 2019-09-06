cb7464fc47fe36f81e66e437d3ae8d3acda6d9f5
wangqiulin <wangqiulin@netwisdom.cn>
2019-09-06 10:32:09 +0800

    建造者模式

--- wql-bizserviceapp-user/bizserviceapp-user/src/main/java/com/wql/cloud/userservice/pojo/domain/User.java
+++ wql-bizserviceapp-user/bizserviceapp-user/src/main/java/com/wql/cloud/userservice/pojo/domain/User.java
@@ -8,17 +8,49 @@ import com.wql.cloud.basic.datasource.baseservice.BaseDO;
 
 import cn.afterturn.easypoi.excel.annotation.Excel;
 
-@Table(name="t_user")
+@Table(name = "t_user")
 public class User extends BaseDO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Excel(name = "姓名", orderNum = "0", width = 15.0)
 	private String userName;
-	
+
 	@Excel(name = "密码", orderNum = "1", width = 30.0)
 	private String userPwd;
 
+	static class UserBuilder {
+		private String userName;
+		private String userPwd;
+
+		public UserBuilder setUserName(String userName) {
+			this.userName = userName;
+			return this;
+		}
+
+		public UserBuilder setUserPwd(String userPwd) {
+			this.userPwd = userPwd;
+			return this;
+		}
+
+		public String getUserName() {
+			return userName;
+		}
+
+		public String getUserPwd() {
+			return userPwd;
+		}
+
+		public User builder(){
+			return new User(this);
+		}
+	}
+
+	public User(UserBuilder builder) {
+		this.userName = builder.getUserName();
+		this.userPwd = builder.getUserPwd();
+	}
+	
 	public String getUserName() {
 		return userName;
 	}
@@ -35,9 +67,12 @@ public class User extends BaseDO implements Serializable {
 		this.userPwd = userPwd;
 	}
 
-	@Override
-	public String toString() {
-		return "User [userName=" + userName + ", userPwd=" + userPwd + "]";
+	public static void main(String[] args) {
+		new User.UserBuilder().setUserName("你好").builder();
+		new User.UserBuilder()
+				.setUserName("你好")
+				.setUserPwd("123")
+				.builder();
 	}
-	
+
 }
