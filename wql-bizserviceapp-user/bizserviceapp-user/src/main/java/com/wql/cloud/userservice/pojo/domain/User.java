98f99ad4f7ca4f1a5b09dcff60b825f3fae72780
wangqiulin <wangqiulin@netwisdom.cn>
2019-09-12 17:24:41 +0800

    delete

--- wql-bizserviceapp-user/bizserviceapp-user/src/main/java/com/wql/cloud/userservice/pojo/domain/User.java
+++ wql-bizserviceapp-user/bizserviceapp-user/src/main/java/com/wql/cloud/userservice/pojo/domain/User.java
@@ -19,38 +19,6 @@ public class User extends BaseDO implements Serializable {
 	@Excel(name = "密码", orderNum = "1", width = 30.0)
 	private String userPwd;
 
-	static class UserBuilder {
-		private String userName;
-		private String userPwd;
-
-		public UserBuilder setUserName(String userName) {
-			this.userName = userName;
-			return this;
-		}
-
-		public UserBuilder setUserPwd(String userPwd) {
-			this.userPwd = userPwd;
-			return this;
-		}
-
-		public String getUserName() {
-			return userName;
-		}
-
-		public String getUserPwd() {
-			return userPwd;
-		}
-
-		public User builder(){
-			return new User(this);
-		}
-	}
-
-	public User(UserBuilder builder) {
-		this.userName = builder.getUserName();
-		this.userPwd = builder.getUserPwd();
-	}
-	
 	public String getUserName() {
 		return userName;
 	}
@@ -65,14 +33,6 @@ public class User extends BaseDO implements Serializable {
 
 	public void setUserPwd(String userPwd) {
 		this.userPwd = userPwd;
-	}
-
-	public static void main(String[] args) {
-		new User.UserBuilder().setUserName("你好").builder();
-		new User.UserBuilder()
-				.setUserName("你好")
-				.setUserPwd("123")
-				.builder();
 	}
 
 }
