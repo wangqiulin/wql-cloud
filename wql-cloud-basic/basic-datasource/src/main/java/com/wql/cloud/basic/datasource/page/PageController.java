0855150dd13f41bd27e8c8afc6de4248be203754
wangqiulin <wangqiulin@netwisdom.cn>
2019-12-05 10:14:32 +0800

    通用页面跳转

---
+++ wql-cloud-basic/basic-datasource/src/main/java/com/wql/cloud/basic/datasource/page/PageController.java
@@ -0,0 +1,32 @@
+package com.wql.cloud.basic.datasource.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 通用页面跳转
+ * 
+ * @author wangqiulin
+ *
+ */
+@Controller
+public class PageController {
+
+	@RequestMapping(value="{page}", method= RequestMethod.GET)
+    public String page(@PathVariable("page") String page){
+        return page;
+    }
+	
+	@RequestMapping(value="{page}/{page2}", method= RequestMethod.GET)
+    public String page(@PathVariable("page") String page, @PathVariable("page2") String page2){
+        return page+"/"+page2;
+    }
+
+	@RequestMapping(value="{page}/{page2}/{page3}", method= RequestMethod.GET)
+    public String page(@PathVariable("page") String page, @PathVariable("page2") String page2, @PathVariable("page3") String page3){
+        return page+"/"+page2+"/"+page3;
+    }
+	
+}
