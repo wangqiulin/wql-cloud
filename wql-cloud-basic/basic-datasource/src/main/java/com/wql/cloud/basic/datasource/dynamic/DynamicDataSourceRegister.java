e69035b42b2d343085885968328f4b946c818803
wangqiulin <wangqiulin@netwisdom.cn>
2019-12-19 16:20:56 +0800

    修改配置

--- wql-cloud-basic/basic-datasource/src/main/java/com/wql/cloud/basic/datasource/dynamic/DynamicDataSourceRegister.java
+++ wql-cloud-basic/basic-datasource/src/main/java/com/wql/cloud/basic/datasource/dynamic/DynamicDataSourceRegister.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.MutablePropertyValues;
@@ -38,7 +39,16 @@ public class DynamicDataSourceRegister implements ImportBeanDefinitionRegistrar,
 	private DataSource defaultDataSource;
 
 	private Map<String, DataSource> customDataSources = new HashMap<String, DataSource>();
-
+	
+	/**
+	 * 加载多数据源配置
+	 */
+	@Override
+	public void setEnvironment(Environment env) {
+		initDefaultDataSource(env);
+		initCustomDataSources(env);
+	}
+	
 	@Override
 	public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
 		Map<Object, Object> targetDataSources = new HashMap<Object, Object>();
@@ -59,10 +69,46 @@ public class DynamicDataSourceRegister implements ImportBeanDefinitionRegistrar,
 		mpv.addPropertyValue("defaultTargetDataSource", defaultDataSource);
 		mpv.addPropertyValue("targetDataSources", targetDataSources);
 		registry.registerBeanDefinition("dataSource", beanDefinition);
-
 		logger.info("Dynamic DataSource Registry");
 	}
 
+
+	/**
+	 * 初始化主数据源
+	 */
+	private void initDefaultDataSource(Environment env) {
+		// 读取主数据源
+		RelaxedPropertyResolver propertyResolver = new RelaxedPropertyResolver(env, "spring.datasource.");
+		Map<String, Object> dsMap = new HashMap<String, Object>();
+		dsMap.put("type", propertyResolver.getProperty("type"));
+		dsMap.put("driver-class-name", propertyResolver.getProperty("driver-class-name"));
+		dsMap.put("url", propertyResolver.getProperty("url"));
+		dsMap.put("username", propertyResolver.getProperty("username"));
+		dsMap.put("password", propertyResolver.getProperty("password"));
+		dsMap.put("publicKey", propertyResolver.getProperty("publicKey"));
+		defaultDataSource = buildDataSource(dsMap);
+		dataBinder(defaultDataSource, env);
+	}
+
+	/**
+	 * 初始化更多数据源
+	 */
+	private void initCustomDataSources(Environment env) {
+		// 读取配置文件获取更多数据源，也可以通过defaultDataSource读取数据库获取更多数据源
+		RelaxedPropertyResolver propertyResolver = new RelaxedPropertyResolver(env, "custom.datasource.");
+		String dsPrefixs = propertyResolver.getProperty("names");
+		if(StringUtils.isBlank(dsPrefixs)) {
+			return ;
+		}
+		for (String dsPrefix : dsPrefixs.split(",")) {// 多个数据源
+			Map<String, Object> dsMap = propertyResolver.getSubProperties(dsPrefix + ".");
+			DataSource ds = buildDataSource(dsMap);
+			customDataSources.put(dsPrefix, ds);
+			dataBinder(ds, env);
+		}
+	}
+	
+	
 	/**
 	 * 创建DataSource
 	 *
@@ -83,16 +129,18 @@ public class DynamicDataSourceRegister implements ImportBeanDefinitionRegistrar,
 
 			Class<? extends DataSource> dataSourceType;
 			dataSourceType = (Class<? extends DataSource>) Class.forName((String) type);
-
+			
 			String driverClassName = dsMap.get("driver-class-name").toString();
 			String url = dsMap.get("url").toString();
 			String username = dsMap.get("username").toString();
 			String password = dsMap.get("password").toString();
 			String publicKey = dsMap.get("publicKey").toString();
-			//使用公钥解密
-			password = ConfigTools.decrypt(publicKey, password);
-			if(logger.isDebugEnabled()) {
-				logger.debug("db-密码：" + password);
+			if(StringUtils.isNotBlank(publicKey)) {
+				//使用公钥解密
+				password = ConfigTools.decrypt(publicKey, password);
+				if(logger.isDebugEnabled()) {
+					logger.debug("数据库密码：" + password);
+				}
 			}
 			DataSourceBuilder factory = DataSourceBuilder.create()
 					.driverClassName(driverClassName)
@@ -106,33 +154,8 @@ public class DynamicDataSourceRegister implements ImportBeanDefinitionRegistrar,
 		}
 		return null;
 	}
-
-	/**
-	 * 加载多数据源配置
-	 */
-	@Override
-	public void setEnvironment(Environment env) {
-		initDefaultDataSource(env);
-		initCustomDataSources(env);
-	}
-
-	/**
-	 * 初始化主数据源
-	 */
-	private void initDefaultDataSource(Environment env) {
-		// 读取主数据源
-		RelaxedPropertyResolver propertyResolver = new RelaxedPropertyResolver(env, "spring.datasource.");
-		Map<String, Object> dsMap = new HashMap<String, Object>();
-		dsMap.put("type", propertyResolver.getProperty("type"));
-		dsMap.put("driver-class-name", propertyResolver.getProperty("driver-class-name"));
-		dsMap.put("url", propertyResolver.getProperty("url"));
-		dsMap.put("username", propertyResolver.getProperty("username"));
-		dsMap.put("password", propertyResolver.getProperty("password"));
-		dsMap.put("publicKey", propertyResolver.getProperty("publicKey"));
-		defaultDataSource = buildDataSource(dsMap);
-		dataBinder(defaultDataSource, env);
-	}
-
+	
+	
 	/**
 	 * 为DataSource绑定更多数据
 	 *
@@ -160,21 +183,7 @@ public class DynamicDataSourceRegister implements ImportBeanDefinitionRegistrar,
 		}
 		dataBinder.bind(dataSourcePropertyValues);
 	}
-
-	/**
-	 * 初始化更多数据源
-	 */
-	private void initCustomDataSources(Environment env) {
-		// 读取配置文件获取更多数据源，也可以通过defaultDataSource读取数据库获取更多数据源
-		RelaxedPropertyResolver propertyResolver = new RelaxedPropertyResolver(env, "custom.datasource.");
-		String dsPrefixs = propertyResolver.getProperty("names");
-		for (String dsPrefix : dsPrefixs.split(",")) {// 多个数据源
-			Map<String, Object> dsMap = propertyResolver.getSubProperties(dsPrefix + ".");
-			DataSource ds = buildDataSource(dsMap);
-			customDataSources.put(dsPrefix, ds);
-			dataBinder(ds, env);
-		}
-	}
+	
 	
 	
 	/**
