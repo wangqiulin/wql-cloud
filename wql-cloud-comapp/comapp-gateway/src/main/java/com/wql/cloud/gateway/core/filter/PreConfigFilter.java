c22540ed680ba0c2ad17892464f6ac8e41d53e6a
wangqiulin <wangqiulin@netwisdom.cn>
2019-12-20 17:41:30 +0800

    nothing

--- wql-cloud-comapp/comapp-gateway/src/main/java/com/wql/cloud/gateway/core/filter/PreConfigFilter.java
+++ wql-cloud-comapp/comapp-gateway/src/main/java/com/wql/cloud/gateway/core/filter/PreConfigFilter.java
@@ -14,7 +14,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
