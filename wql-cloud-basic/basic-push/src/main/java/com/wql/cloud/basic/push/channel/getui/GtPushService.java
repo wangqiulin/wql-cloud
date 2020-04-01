240e1d17b95d6a8708d7abbec602539d61ed258c
wangqiulin <wangqiulin@netwisdom.cn>
2020-04-01 11:00:19 +0800

    delete

--- wql-cloud-basic/basic-push/src/main/java/com/wql/cloud/basic/push/channel/getui/GtPushService.java
+++
@@ -1,9 +0,0 @@
-package com.wql.cloud.basic.push.channel.getui;
-
-import com.wql.cloud.basic.push.channel.getui.template.GtPushTemplate;
-import com.wql.cloud.basic.push.channel.getui.vo.GetuiDataResult;
-import com.wql.cloud.basic.push.route.PushService;
-
-public interface GtPushService extends PushService<GetuiDataResult, GtPushTemplate> {
-
-}
--- wql-cloud-basic/basic-push/src/main/java/com/wql/cloud/basic/push/channel/meizu/FlymePushService.java
+++
@@ -1,9 +0,0 @@
-package com.wql.cloud.basic.push.channel.meizu;
-
-import com.wql.cloud.basic.push.channel.meizu.template.FlymePushTemplate;
-import com.wql.cloud.basic.push.channel.meizu.vo.FlymePushDataResult;
-import com.wql.cloud.basic.push.route.PushService;
-
-public interface FlymePushService extends PushService<FlymePushDataResult, FlymePushTemplate> {
-
-}
